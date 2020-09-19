3b8633fc262a675375b6f56007488efc94bb52ad
sekibura <51335422+sekibura@users.noreply.github.com>
2020-09-19 18:10:24 +0300

    Update Controller.java

--- Lab_1/src/Controller.java
+++ Lab_1/src/Controller.java
@@ -1,9 +1,13 @@
 public class Controller {
+    private int CountStrings=10;
 
     public void Control(){
         String[] array={"Xyz","Yxz","Abc","Bcd"};
+
         Model model=new Model();
         String[] result=model.SortString(array);
+        View view=new View();
+        view.Display(result);
 
     }
 }
