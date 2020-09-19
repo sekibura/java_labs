c2eb95d0d7c300fea0cc9256ffd4a75d633c3876
sekibura <51335422+sekibura@users.noreply.github.com>
2020-09-19 18:18:06 +0300

    Update Controller.java

--- Lab_1/src/Controller.java
+++ Lab_1/src/Controller.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 public class Controller {
     private int CountStrings=10;
 
     public void Control(){
-        String[] array={"Xyz","Yxz","Abc","Bcd"};
+        Scanner scaner = new Scanner(System.in);  // Create a Scanner object
+        String[] array=new String[CountStrings];
+
+        for (int i=0;i<CountStrings;i++){
+            System.out.println("Input string №"+i);
+            array[i]=scaner.nextLine().toString();
+        }
 
         Model model=new Model();
         String[] result=model.SortString(array);
