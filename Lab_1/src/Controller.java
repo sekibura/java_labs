e04f43e008eaf084bb2860554a76e86159bfb99b
sekibura <51335422+sekibura@users.noreply.github.com>
2020-09-20 15:03:46 +0300

    add check empty string

--- Lab_1/src/Controller.java
+++ Lab_1/src/Controller.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 public class Controller {
-    private int CountStrings=10;
+    private int CountStrings=2; // Number of strings in array
 
     public void Control(){
         Scanner scaner = new Scanner(System.in);  // Create a Scanner object
-        String[] array=new String[CountStrings];
+        String[] array=new String[CountStrings];  // Create array of strings
 
+        // Get value for each string in array
         for (int i=0;i<CountStrings;i++){
             System.out.println("Input string №"+i);
             array[i]=scaner.nextLine().toString();
+            if(array[i].isEmpty()){ // Check empty string
+                i--;
+                System.err.println("String is empty!"); // Error of empty string
+            }
         }
-
-        Model model=new Model();
-        String[] result=model.SortString(array);
-        View view=new View();
-        view.Display(array);
-        view.Display(result);
+        View view=new View(); //Create a View object
+        view.Display(array);  //Display unsorted array
+        Model model=new Model(); //Create a Model Object
+        String[] result=model.SortString(array); // Use Model for sort array
+        view.Display(result); // Display result of models work
 
     }
 }
